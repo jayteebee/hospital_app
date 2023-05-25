b9ec563cd532151548cd243d83424f755b808400
Jethro_Block <jethroblock22@gmail.COM>
2023-05-25 18:54:28 +0100

    Forgot to seed!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Patient.create(first_name: "Jethro", last_name: "Block", diagnosis: "JavaScript", born_on: Date.new(1980, 1, 1))
+Patient.create(first_name: "Jethro", last_name: "Block", diagnosis: "JavaScript", born_on: Date.new(1980, 1, 1))
+Patient.create(first_name: "Janet", last_name: "Smith", diagnosis: "Python", born_on: Date.new(1975, 2, 14))
+Patient.create(first_name: "James", last_name: "Johnson", diagnosis: "Java", born_on: Date.new(1990, 5, 25))
+Patient.create(first_name: "Sophia", last_name: "Brown", diagnosis: "C#", born_on: Date.new(1988, 7, 18))
+Patient.create(first_name: "Olivia", last_name: "Davis", diagnosis: "C++", born_on: Date.new(1982, 10, 30))
+Patient.create(first_name: "Daniel", last_name: "Miller", diagnosis: "Ruby", born_on: Date.new(1972, 11, 21))
+Patient.create(first_name: "Michael", last_name: "Wilson", diagnosis: "Rust", born_on: Date.new(1999, 12, 12))
+Patient.create(first_name: "Emma", last_name: "Moore", diagnosis: "Swift", born_on: Date.new(1979, 3, 9))
+Patient.create(first_name: "Liam", last_name: "Taylor", diagnosis: "Go", born_on: Date.new(1985, 8, 15))
+Patient.create(first_name: "Noah", last_name: "Anderson", diagnosis: "Kotlin", born_on: Date.new(1987, 9, 20))
